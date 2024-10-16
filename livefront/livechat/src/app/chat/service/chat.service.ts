43e564ed0640dcf737028086501aeb60373f7c2f
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-10-16 08:30:33 -0300

    Update chat.service.ts

--- livefront/livechat/src/app/chat/service/chat.service.ts
+++ livefront/livechat/src/app/chat/service/chat.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Client, IStompSocket } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
 
@@ -10,18 +10,28 @@ export interface ChatMessage {
   type: string;
 }
 
+export interface ConnectionStatus {
+  connected: boolean;
+  error?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
   private client: Client;
-  private messagesSubject = new BehaviorSubject<ChatMessage>({} as ChatMessage);
+  private messagesSubject = new BehaviorSubject<ChatMessage[]>([]);
+  private connectionStatusSubject = new BehaviorSubject<ConnectionStatus>({
+    connected: false
+  });
+
   messages$ = this.messagesSubject.asObservable();
+  connectionStatus$ = this.connectionStatusSubject.asObservable();
 
   constructor() {
     this.client = new Client({
       webSocketFactory: () => {
-        return new SockJS('/livechat-websocket') as IStompSocket;
+        return new SockJS('http://localhost:8080/livechat-websocket') as IStompSocket;
       },
       debug: (str) => {
         console.log(str);
@@ -33,37 +43,60 @@ export class ChatService {
 
     this.client.onConnect = (frame) => {
       console.log('Connected: ' + frame);
+      this.connectionStatusSubject.next({ connected: true });
+
       this.client.subscribe('/topic/public', message => {
         const chatMessage: ChatMessage = JSON.parse(message.body);
-        this.messagesSubject.next(chatMessage);
+        const currentMessages = this.messagesSubject.value;
+        this.messagesSubject.next([...currentMessages, chatMessage]);
       });
     };
 
     this.client.onStompError = (frame) => {
       console.error('Broker reported error: ' + frame.headers['message']);
       console.error('Additional details: ' + frame.body);
+      this.connectionStatusSubject.next({
+        connected: false,
+        error: frame.headers['message']
+      });
     };
   }
 
-  connect() {
-    this.client.activate();
+  connect(): Observable<void> {
+    return new Observable(subscriber => {
+      try {
+        this.client.activate();
+        subscriber.next();
+        subscriber.complete();
+      } catch (error) {
+        subscriber.error(error);
+      }
+    });
   }
 
   disconnect() {
-    this.client.deactivate();
+    if (this.client.connected) {
+      this.client.deactivate();
+      this.connectionStatusSubject.next({ connected: false });
+      this.messagesSubject.next([]);
+    }
   }
 
   sendMessage(user: string, message: string) {
-    this.client.publish({
-      destination: "/app/chat.sendMessage",
-      body: JSON.stringify({ user, message })
-    });
+    if (this.client.connected) {
+      this.client.publish({
+        destination: "/app/chat.sendMessage",
+        body: JSON.stringify({ user, message })
+      });
+    }
   }
 
   joinChat(username: string) {
-    this.client.publish({
-      destination: "/app/chat.addUser",
-      body: JSON.stringify({ user: username, message: null })
-    });
+    if (this.client.connected) {
+      this.client.publish({
+        destination: "/app/chat.addUser",
+        body: JSON.stringify({ user: username, message: null })
+      });
+    }
   }
 }
