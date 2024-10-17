74c638a897644b3dfcffdc95fd07516bf478b173
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-10-17 14:51:10 -0300

    Update ChatOutput.java

--- livechat/src/main/java/livechat/app/livechat/domain/ChatOutput.java
+++ livechat/src/main/java/livechat/app/livechat/domain/ChatOutput.java
@@ -3,10 +3,4 @@ package livechat.app.livechat.domain;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public record ChatOutput(String user, String content, String time, String type) {
-
-    // Construtor alternativo para definir o time e o type automaticamente
-    public ChatOutput(String user, String content) {
-        this(user, content, new SimpleDateFormat("HH:mm").format(new Date()), "CHAT");
-    }
-}
+public record ChatOutput(String content) {}
