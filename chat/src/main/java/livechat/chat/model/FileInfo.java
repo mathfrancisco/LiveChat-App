d3702ba87347f715e6e1bef6a59dd24a4e237f22
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-10-22 17:13:18 -0300

    Create FileInfo.java

---
+++ chat/src/main/java/livechat/chat/model/FileInfo.java
@@ -0,0 +1,11 @@
+package livechat.chat.model;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileInfo {
+    private String fileName;
+    private String fileType;
+    private String fileUrl;
+    private long fileSize;
+}
